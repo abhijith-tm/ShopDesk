e5bd2abb2bc4c0353d379781699dcfbddd3b24e2
abhijith-tm <abhijithtm20@gmail.com>
2026-08-19 22:46:45 +0530

    feat: add product API URL routing

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,11 +3,14 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import ProductSerializer
 from .models import Product
+
+# /api/products/  Get ->list all. POST-> create
+
 class ProductListCreateView(ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
-
+#/api/products/<id>/  GET    → retrieve one , PUT    → update, PATCH  → partial update,  DELETE → delete
 class ProductRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
