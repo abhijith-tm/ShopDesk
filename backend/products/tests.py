982c5915edeff8c563ad095280b4c87cff80aa50
abhijith-tm <abhijithtm20@gmail.com>
2026-08-20 21:45:35 +0530

    started working on product API tests

--- backend/products/tests.py
+++ backend/products/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+
 
 # Create your tests here.
+class ProductTestCase(TestCase):
+
+    def setUp(self):
+            self.client = APIClient()
+
+    def test_product_list_empty(self):
+        response = self.client.get("/api/products/")
+        self.assertEqual(response.data,[])
+        self.assertEqual(response.status_code,200)
+
+    def test_product_creation(self):
+        response = self.client.post("/api/products/", {'name': 'Parle G','cost_price': '6','selling_price': '10','stock_quantity': '150'} ,format='json')
+        self.assertEqual(response.data,{'name': 'Parle G''cost_price': '6''selling_price': '10''stock_quantity': 150},format='json')
+        self.assertEqual(response.status_code,201)
