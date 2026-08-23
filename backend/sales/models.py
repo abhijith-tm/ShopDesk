311d610b924732fa1ed60a9d153a8cb513c94219
abhijith-tm <abhijithtm20@gmail.com>
2026-08-23 13:15:24 +0530

    feat: add sale and sale item models

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -1,6 +1,74 @@
 from django.db import models
+from products.models import Product
+from django.core.validators import MinValueValidator
+from django.db.models import Q
 
 # Create your models here.
 
 class Sale(models.Model):
-    
+
+    class Status(models.TextChoices):
+      PENDING = 'pending'
+      COMPLETED = 'completed'
+      CANCELLED = 'cancelled'
+
+
+    status = models.CharField(
+       max_length=9,
+       choices=Status.choices,
+       default=Status.PENDING
+    )
+
+    created_at = models.DateTimeField(
+       auto_now_add=True
+    )
+
+class SaleItem(models.Model):
+	sale = models.ForeignKey(
+		to=Sale, 
+		on_delete=models.CASCADE # saleItem gets deleted when sale gets deleted
+		,related_name="items" # so we can do sale.items.all() instead of sale.saleitem_set.all()
+	)
+
+	product = models.ForeignKey(
+		to=Product,
+		on_delete=models.PROTECT #prevents delete
+
+	)
+
+	product_name = models.CharField(
+	max_length=200,
+
+	)
+
+	quantity = models.IntegerField(
+		default=1,
+		validators=[MinValueValidator(1)]
+	)
+
+	unit_price = models.DecimalField(
+		max_digits=8,
+		decimal_places=2,
+		validators=[MinValueValidator(0)]
+	)
+
+	cost_price = models.DecimalField(
+		max_digits=8,
+		decimal_places=2,
+		validators=[MinValueValidator(0)]
+	)
+	class Meta:
+          constraints = [
+              models.CheckConstraint(
+                condition=Q(quantity__gte=1),
+				name="quantity_gteater_than_1"
+			  ),
+ 			  models.CheckConstraint(
+				condition=Q(unit_price__gte=0),
+				name="unit_price_gteater_than_0"
+			  ),
+     		  models.CheckConstraint(
+					condition=Q(cost_price__gte=0),
+					name="cost_price_gteater_than_0 "
+			  )
+		  ]
