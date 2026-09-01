8acd71a3910a36b9d47a2e72993ff6e4cb515319
abhijith-tm <abhijithtm20@gmail.com>
2026-09-01 21:56:51 +0530

    product api

--- frontend/src/API/product.jsx
+++ frontend/src/API/product.jsx
@@ -1,14 +1,19 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useEffect } from 'react';
-const GetProduct = () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        axios
-            .get("http://localhost:8000/api/products/")
-            .then((response) => setData(response.data))
-            .catch((err) => console.error(err));
-    }, []);
-    return <pre>{JSON.stringify(data, null, 2)}</pre>;
-};
-export default GetProduct;
+// import axios from 'axios';
+// import { useState } from 'react';
+// import { useEffect } from 'react';
+// const GetProduct = () => {
+//     const [data, setData] = useState([]);
+//     useEffect(() => {
+//         axios
+//             .get("http://localhost:8000/api/products/")
+//             .then((response) => setData(response.data))
+//             .catch((err) => console.error(err));
+//     }, []);
+//     return <pre>{JSON.stringify(data, null, 2)}</pre>;
+// };
+// export default GetProduct;
+
+
+function hello(){
+    
+}
